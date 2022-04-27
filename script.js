fde8859bf4ecfcf76d37ea4ce0a792e82f95313a
Elodin77 <23061402@student.uwa.edu.au>
2022-04-27 17:50:47 +0800

    added pages

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 var currentPage = 0;
+var pages = ["views/home.html", "views/portfolio.html", "views/contact.html"];
 
 function onload() {
   // Add navbar
   generateNavBar();
-  // Generate page title based on file name
-  let title = window.location.pathname.split("/").pop().slice(0,-5) || "home";
-  document.title = title.substring(0,1).toUpperCase() + title.substring(1);
+  
 }
 
 // Updates elements on the page
-function update() {
-  function updateScrollBar() {
-    let colors = ["#A40011", "#11A400", "#6300d8"];
-    let winHeight = $(window).height();
-    let docHeight = $(document).height();
-    let scrollTop = $(window).scrollTop(); //NaN or zero at top
-    let trackLength = docHeight - winHeight;
-    let pctScrolled = Math.round(scrollTop / trackLength * 100);
-    document.getElementById("scroll-progress-bar").style.width = pctScrolled + "%";
-    document.getElementById("scroll-progress-bar").style.background = "linear-gradient(to right, #1e2530, " + colors[currentPage] + " 100%)";
-  }
+function updateContent() {
+  // Update the content div
+  fetch(pages[currentPage])
+  .then(response => response.text())
+  .then(text => document.getElementById("content").innerHTML = text);
 
-  // update() functionality
+  // Update the page title
+  let title = pages[currentPage].split("/").pop().slice(0, -5) || "home";
+  document.title = "DJ " + title.substring(0, 1).toUpperCase() + title.substring(1);
+
+  // Update the scrollbar -> different colour for each page
   updateScrollBar();
+}
+
+function updateScrollBar() {
+  let colors = ["#A40011", "#11A400", "#6300d8"];
+  let winHeight = $(window).height();
+  let docHeight = $(document).height();
+  let scrollTop = $(window).scrollTop(); //NaN or zero at top
+  let trackLength = docHeight - winHeight;
+  let pctScrolled = Math.round(scrollTop / trackLength * 100);
+  document.getElementById("scroll-progress-bar").style.width = pctScrolled + "%";
+  document.getElementById("scroll-progress-bar").style.background = "linear-gradient(to right, #1e2530, " + colors[currentPage] + " 100%)";
 }
 
 // Does necessary javascript for navbar generation
@@ -30,11 +38,12 @@ function generateNavBar() {
   navLinks = document.getElementById("nav-links");
   navLinks.querySelectorAll("a").forEach((link) => link.addEventListener("click", (event) => {
     currentPage = Array.from(event.target.parentNode.children).indexOf(event.target);
-    update();
+    updateContent();
   }, false));
+  updateContent(); // Call once for initial update
 
-  document.addEventListener("scroll", update, false);
-
-  update(); // Call once for initial update
+  document.addEventListener("scroll", updateScrollBar, false);
+  updateScrollBar(); // Call once for initial update
+  
 }
 
